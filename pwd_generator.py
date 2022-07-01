ad57290a5564faf1ecf3647476e3554c79fc8f89
liferacer333 <68821086+liferacer333@users.noreply.github.com>
2022-07-01 13:54:22 +0530

    Update pwd_generator.py

--- pwd_generator.py
+++ pwd_generator.py
@@ -19,9 +19,16 @@ print("Welcome to Random Password Generator :)")
 characters = string.ascii_letters + string.digits + string.punctuation
 passwords = int(input("Enter the number of passwords you want to generate: "))
 #generates n no.of passwords according to user input
+
 length = int(input("Enter your password length: "))
 #password length
 
+file = "passwords.txt"
+#passwords are saved in the passwords.txt file
+
+print("Your passwords are saved in the",file, "file")
+sys.stdout = open(file, "w")
+print("Your passwords: \n")
 print("Your passwords:")
 
 for pwd in range(passwords):
